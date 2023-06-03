7604aa4383e8576e0f04b8a4fc55387469c59003
Mohamed Hossam <nitrosmm@gmail.com>
2023-06-04 00:35:59 +0300

    Adding Notification To add Post
    Signed-off-by: Mohamed Hossam <nitrosmm@gmail.com>

--- lib/view/screen/add_post.dart
+++ lib/view/screen/add_post.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:balghny/view/screen/com.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,24 +10,56 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
 class Add_post extends StatefulWidget {
-
-
-final File img;
-  const Add_post({Key? key,required this.img}) : super(key: key);
+  final File img;
+  const Add_post({Key? key, required this.img}) : super(key: key);
 
   @override
   State<Add_post> createState() => _Add_postState();
 }
 
 class _Add_postState extends State<Add_post> {
-
-  String location ='Null, Press Button';
+  String location = 'Null, Press Button';
   String Address = 'search';
 
+  int postCount = 1;
 
-  
+  void addPostte() {
+    setState(() {
+      postCount++;
+      print('Posts: $postCount');
+    });
+  }
+
+  void initState() {
+    AwesomeNotifications().initialize(
+        null,
+        [
+          NotificationChannel(
+              channelKey: "Basic_channel",
+              channelName: "Basic Notification",
+              channelDescription: "Notification channel For Basic tests")
+        ],
+        debug: true);
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+    super.initState();
+  }
+
+  triggerNotification() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 10,
+          channelKey: "Basic_channel",
+          title: "simple Notification",
+          body: "This is a basic notification"),
+    );
+  }
+
   TextEditingController addpost = TextEditingController();
-  bool isloading =false ;
+  bool isloading = false;
   var img_url;
 
   //////////location fun
@@ -49,19 +82,17 @@ class _Add_postState extends State<Add_post> {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
   }
 
-
-
-  Future<void> GetAddressFromLatLong(Position position)async {
-    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+  Future<void> GetAddressFromLatLong(Position position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
     print(placemarks);
     Placemark place = placemarks[0];
     Address = ' ${place.street},  ';
-    setState(()  {
-    });
-
+    setState(() {});
 
     //${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}
   }
@@ -69,48 +100,46 @@ class _Add_postState extends State<Add_post> {
 
   ///
 
-
-Future<void> _saveUserDataToFirestore(String? post_body) async {
-
-
+  Future<void> _saveUserDataToFirestore(String? post_body) async {
     //////////
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
-  final String? myPhotoUrl = FirebaseAuth.instance.currentUser?.photoURL;
-  Reference storageRef = FirebaseStorage.instance.ref().child("images_cate/${DateTime.now().toString()}");
-  UploadTask uploadTask = storageRef.putFile(widget.img);
-  TaskSnapshot snapshot = await uploadTask;
-  String imageUrl = await snapshot.ref.getDownloadURL();
-  //final String? userName = FirebaseAuth.instance.currentUser?.displayName;
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    final String? myPhotoUrl = FirebaseAuth.instance.currentUser?.photoURL;
+    Reference storageRef = FirebaseStorage.instance
+        .ref()
+        .child("images_cate/${DateTime.now().toString()}");
+    UploadTask uploadTask = storageRef.putFile(widget.img);
+    TaskSnapshot snapshot = await uploadTask;
+    String imageUrl = await snapshot.ref.getDownloadURL();
+    //final String? userName = FirebaseAuth.instance.currentUser?.displayName;
 
-  // Retrieve additional data from the 'users' collection
-  DocumentSnapshot userSnapshot = await firestore.collection('users').doc(FirebaseAuth.instance.currentUser?.uid.toString()).get();
+    // Retrieve additional data from the 'users' collection
+    DocumentSnapshot userSnapshot = await firestore
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser?.uid.toString())
+        .get();
 
-  if (userSnapshot.exists) {
-    // User document exists in the 'users' collection
-    final userData = userSnapshot.data() as Map<String, dynamic>;
-    final String? username = userData['name'];
-    final String? userPhotoUrl = userData['photourl'];
+    if (userSnapshot.exists) {
+      // User document exists in the 'users' collection
+      final userData = userSnapshot.data() as Map<String, dynamic>;
+      final String? username = userData['name'];
+      final String? userPhotoUrl = userData['photourl'];
 
-    // Add document to 'posts' collection
-    DocumentReference postDocRef = await firestore.collection('posts').add(
-      {
+      // Add document to 'posts' collection
+      DocumentReference postDocRef = await firestore.collection('posts').add({
         'post_body': post_body,
         'time': DateTime.now(),
         'username': username,
         'photourl': userPhotoUrl,
         'imageUrl': imageUrl,
         'Address': Address,
-
-
-      }
-    );
-    print('Post added to Firestore.');
-  } else {
-    // User document does not exist in the 'users' collection
-    print('User document not found in the users collection. Cannot add post.');
-}
-}
-
+      });
+      print('Post added to Firestore.');
+    } else {
+      // User document does not exist in the 'users' collection
+      print(
+          'User document not found in the users collection. Cannot add post.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -128,93 +157,123 @@ Future<void> _saveUserDataToFirestore(String? post_body) async {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 50,),
+              SizedBox(
+                height: 50,
+              ),
               Center(
                 child: Stack(
                   children: [
-                    Center(child: Image.file(widget.img,width: 200, height: 200, fit:BoxFit.fill),
+                    Center(
+                      child: Image.file(widget.img,
+                          width: 200, height: 200, fit: BoxFit.fill),
                     )
                   ],
                 ),
               ),
-              SizedBox(height: 20,),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                       Container(
-                           margin: EdgeInsets.only(left: 20),
-                           child: Text("Title",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  SizedBox(width: 350,
-                    child:  Container(margin:EdgeInsets.only(left: 30,top: 5),
-//"Fire-Disaster"
-                        child: Text('${Address}',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))),
-                  ),
-                ],
+              SizedBox(
+                height: 20,
               ),
-              SizedBox(height: 20,),
               Column(
                 children: [
                   Row(
                     children: [
                       Container(
                           margin: EdgeInsets.only(left: 20),
-                          child: Text("Description",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))),
+                          child: Text(
+                            "Title",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          )),
                     ],
                   ),
                   SizedBox(height: 5),
                   SizedBox(
-                    width: 330,height: 100,
+                    width: 350,
+                    child: Container(
+                        margin: EdgeInsets.only(left: 30, top: 5),
+//"Fire-Disaster"
+                        child: Text('${Address}',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold))),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(left: 20),
+                          child: Text("Description",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold))),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    width: 330,
+                    height: 100,
                     child: TextFormField(
                       controller: addpost,
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.symmetric(vertical: 60,horizontal: 10),
-                            border: OutlineInputBorder()),
-
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 60, horizontal: 10),
+                          border: OutlineInputBorder()),
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 20,),
-
+              SizedBox(
+                height: 20,
+              ),
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 20),
                 child: SizedBox(
                   height: 40,
-                  child: 
-                  isloading ? Center(child: CircularProgressIndicator(),):
-                  ElevatedButton(
-                    onPressed: () async{
+                  child: isloading
+                      ? Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : ElevatedButton(
+                          onPressed: () async {
+                            addPostte();
+                            triggerNotification();
+                            //////////
 
-                      //////////
-
-                      if (Address.isNotEmpty) {
-                        Position position = await _getGeoLocationPosition();
-                        location ='Lat: ${position.latitude} , Long: ${position.longitude}';
-                        GetAddressFromLatLong(position);
-                        setState(() {
-                          GetAddressFromLatLong(position);
-                        });
-                        /////
-                        _saveUserDataToFirestore(addpost.text);
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => PostListScreen()),
-                        );
-                      }
-                       },
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.green),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)))),
-                    child: Text(
-                      'Post',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  ),
+                            if (Address.isNotEmpty) {
+                              Position position =
+                                  await _getGeoLocationPosition();
+                              location =
+                                  'Lat: ${position.latitude} , Long: ${position.longitude}';
+                              GetAddressFromLatLong(position);
+                              setState(() {
+                                GetAddressFromLatLong(position);
+                              });
+                              /////
+                              _saveUserDataToFirestore(addpost.text);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PostListScreen()),
+                              );
+                            }
+                          },
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10)))),
+                          child: Text(
+                            'Post',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ),
                 ),
               ),
             ],
--- lib/view/screen/com.dart
+++ lib/view/screen/com.dart
@@ -1,9 +1,12 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:balghny/model/post.dart';
 import 'package:balghny/view/screen/home_page.dart';
 import 'package:balghny/view/screen/profile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wc_flutter_share/wc_flutter_share.dart';
+
+import 'notif.dart';
 //import 'dart:io';
 
 class PostListScreen extends StatefulWidget {
@@ -18,13 +21,6 @@ class _PostListScreenState extends State<PostListScreen> {
   var myName = "";
   var myPhotoUrl; // add this variable to store the user's photo URL
 
-  @override
-  void initState() {
-
-    super.initState();
-  }
-
-
   int currentTab = 0;
   final List<Widget> screens = [
     ProfileScreen(),
@@ -33,17 +29,81 @@ class _PostListScreenState extends State<PostListScreen> {
     HomePage(),
   ];
 
-  Widget currentScreen =  PostListScreen();
+  int postCount = 1;
 
+  void addPostte() {
+    setState(() {
+      postCount++;
+      print('Posts: $postCount');
+    });
+  }
 
+  void initState() {
+    AwesomeNotifications().initialize(
+        null,
+        [
+          NotificationChannel(
+              channelKey: "Basic_channel",
+              channelName: "Basic Notification",
+              channelDescription: "Notification channel For Basic tests")
+        ],
+        debug: true);
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+    super.initState();
+  }
 
+  triggerNotification() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 10,
+          channelKey: "Basic_channel",
+          title: "simple Notification",
+          body: "This is a basic notification"),
+    );
+  }
+
+/*
+AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+            channelKey: "Basic_channel",
+            channelName: "Basic Notification",
+            channelDescription: "Notification channel For Basic tests")
+      ],
+      debug: true);
+    void initState() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+    super.initState();
+  }
+
+  triggerNotification() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(id: 10, channelKey: "Basic_channel",
+      title: "simple Notification", body: "This is a basic notification"),
+    );
+  }
+*/
+
+  Widget currentScreen = PostListScreen();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Community',style: TextStyle(fontWeight: FontWeight.bold),),
+        title: const Text(
+          'Community',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
       ),
       body: SafeArea(
@@ -70,11 +130,14 @@ class _PostListScreenState extends State<PostListScreen> {
                 .map(
                     (doc) => Post.fromJson(doc.data()! as Map<String, dynamic>))
                 .toList();
-            return Container(margin: EdgeInsets.only(bottom: 20),
+            return Container(
+              margin: EdgeInsets.only(bottom: 20),
               child: ListView.separated(
-
-                separatorBuilder: (context,index){
-                  return Divider(color: Colors.grey[200],thickness: 20,);
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: Colors.grey[200],
+                    thickness: 20,
+                  );
                 },
                 itemCount: posts.length,
                 itemBuilder: (BuildContext context, int index) {
@@ -91,11 +154,12 @@ class _PostListScreenState extends State<PostListScreen> {
                       sharePopupTitle: '',
                     );
                   }
+
                   return Wrap(
                     children: [
                       Container(
                         padding: EdgeInsets.all(7),
-                      //   height: 250,
+                        //   height: 250,
                         decoration: BoxDecoration(
                           color: Colors.white,
                         ),
@@ -109,22 +173,24 @@ class _PostListScreenState extends State<PostListScreen> {
                                   child: Container(
                                     margin: EdgeInsets.only(left: 10),
                                     child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(50.0),
-                                        child:
-
-                                        CircleAvatar(//post.photourl != null &&
-                                          backgroundImage: post.photourl.isNotEmpty
+                                        borderRadius:
+                                            BorderRadius.circular(50.0),
+                                        child: CircleAvatar(
+                                          //post.photourl != null &&
+                                          backgroundImage: post
+                                                  .photourl.isNotEmpty
                                               ? NetworkImage(post.photourl)
-                                              : NetworkImage('https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'),
-                                        )
-                                    ),
+                                              : NetworkImage(
+                                                  'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'),
+                                        )),
                                   ),
                                 ),
                                 SizedBox(width: 10),
                                 Expanded(
                                   flex: 5,
                                   child: Container(
-                                    margin: EdgeInsets.only(right: 140,top: 10),
+                                    margin:
+                                        EdgeInsets.only(right: 140, top: 10),
                                     child: Column(
                                       crossAxisAlignment:
                                           CrossAxisAlignment.start,
@@ -136,15 +202,22 @@ class _PostListScreenState extends State<PostListScreen> {
                                               fontSize: 15),
                                         ),
                                         Text(post.time.toString(),
-                                            style: TextStyle(color: Colors.grey))
+                                            style:
+                                                TextStyle(color: Colors.grey))
                                       ],
                                     ),
                                   ),
                                 ),
                                 Expanded(
                                     child: IconButton(
-                                        onPressed: () {},
-                                        icon: Icon(Icons.delete_forever,color: Colors.green,)))
+                                        onPressed: () {
+                                          addPostte();
+                                          triggerNotification();
+                                        },
+                                        icon: Icon(
+                                          Icons.delete_forever,
+                                          color: Colors.red,
+                                        )))
                               ],
                             ),
                             SizedBox(
@@ -152,14 +225,12 @@ class _PostListScreenState extends State<PostListScreen> {
                             ),
                             Container(
                               padding: EdgeInsets.all(5),
-                              child:
-                              Stack(
+                              child: Stack(
                                 children: [
                                   Positioned(
-                                   // top: 0,
-                                  //  left: 0,
+                                    // top: 0,
+                                    //  left: 0,
                                     child: Text("Address: ",
-
                                         style: TextStyle(
                                             fontSize: 15,
                                             fontWeight: FontWeight.bold)),
@@ -168,13 +239,13 @@ class _PostListScreenState extends State<PostListScreen> {
                                     margin: EdgeInsets.only(left: 60),
                                     child: Text(
                                       "${post.Address}",
-                                     // maxLines: 2,
-                                     //   overflow: TextOverflow.ellipsis,
+                                      // maxLines: 2,
+                                      //   overflow: TextOverflow.ellipsis,
                                     ),
                                   ),
                                 ],
                               ),
-                            /*  Row(children: [
+                              /*  Row(children: [
                                 Expanded(
                                   flex: 1,
                                   child: Text("Address: ",
@@ -196,11 +267,14 @@ class _PostListScreenState extends State<PostListScreen> {
                               padding: EdgeInsets.all(5),
                               child: Text(post.post_body),
                             ),
-
                             Center(
                               child: Container(
                                 width: 200,
-                                child: Image.network(post.photocate,fit: BoxFit.cover,height: 100,),
+                                child: Image.network(
+                                  post.photocate,
+                                  fit: BoxFit.cover,
+                                  height: 100,
+                                ),
                               ),
                             ),
                             Center(
@@ -209,6 +283,7 @@ class _PostListScreenState extends State<PostListScreen> {
                                     onPressed: () async {
                                       _shareContent(context);
                                       print("object");
+                                      addPostte();
                                     },
                                     child: Text("Facebook Share",
                                         style: TextStyle(
@@ -260,35 +335,52 @@ class _PostListScreenState extends State<PostListScreen> {
                   Navigator.of(context).pushNamed("post");
                 },
                 icon: Icon(Icons.groups,
-                    color: currentTab == 2
-                        ? Colors.green
-                        : Colors.black),
+                    color: currentTab == 2 ? Colors.green : Colors.black),
               ),
               SizedBox(
                 width: 10,
               ),
               IconButton(
                 onPressed: () {
-                   Navigator.of(context).pushNamed("notification");
-
-
-
+                  Navigator.of(context).pushNamed("notification");
                 },
-                icon: Icon(Icons.notifications_active,
-                    color: currentTab == 4
-                        ? Colors.green
-                        : Colors.black),
+                icon: Stack(
+                  children: [
+                    Icon(Icons.notifications),
+                    postCount > 0
+                        ? Positioned(
+                            right: 0,
+                            child: Container(
+                              padding: EdgeInsets.all(2),
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              constraints: BoxConstraints(
+                                minWidth: 16,
+                                minHeight: 16,
+                              ),
+                              child: Text(
+                                postCount.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          )
+                        : SizedBox.shrink(),
+                  ],
+                ),
               ),
 
               IconButton(
                 onPressed: () {
                   Navigator.of(context).pushNamed("profile");
-
                 },
                 icon: Icon(Icons.person,
-                    color: currentTab == 5
-                        ? Colors.green
-                        : Colors.black),
+                    color: currentTab == 5 ? Colors.green : Colors.black),
               ),
             ],
           ),
